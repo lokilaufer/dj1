3676786ecd652e23da52733bbd72d34231991099
Elena Lopatina <lokila667@gmail.com>
2024-11-20 19:13:41 +0400

    first commit

---
+++ view.py
@@ -0,0 +1,31 @@
+import os
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    workdir_contents = os.listdir('.')
+    msg = f'Содержимое рабочей директории: {workdir_contents}'
+    return HttpResponse(msg)
